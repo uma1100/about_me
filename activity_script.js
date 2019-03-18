762869c6d3ffadea79dd2eb2b37f886ace729765
yuma yuma <yumayuma0322@gmail.com>
2019-03-19 00:53:28 +0900

    add info

---
+++ activity_script.js
@@ -0,0 +1,35 @@
+$(function () { 
+	$('#act_view_1').click(function(e) {
+		$('main').fadeOut(function () { 
+			$('#back-curtain')
+			.css({
+				'width' : $(window).width(),    // ウィンドウ幅
+				'height': $(window).height()    // 同 高さ
+			})
+			.show(function () { 
+				$('#act_1').fadeIn();
+			 });
+		 });
+		 $('.mdl-layout--small-screen-only').hide();
+	});
+	$('#act_view_2').click(function(e) {
+		$('main').fadeOut(function () { 
+			$('#back-curtain')
+			.css({
+				'width' : $(window).width(),    // ウィンドウ幅
+				'height': $(window).height()    // 同 高さ
+			})
+			.show(function () { 
+				$('#act_2').fadeIn();
+			 });
+		 });
+		 $('.mdl-layout--small-screen-only').hide();
+	});
+	// ④暗幕と拡大画像を非表示
+	$('#back-curtain,button').on('click',function() {
+		$('.act_article').fadeOut( 
+			function() {$('#back-curtain').hide(function () { $('main').fadeIn(); });}
+		);
+		$('.mdl-layout--small-screen-only').fadeIn();
+	});
+ });
