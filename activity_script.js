23207dbf2ca13c8c521718104fdbacdb4c939efa
yuma yuma <yumayuma0322@gmail.com>
2019-03-31 23:20:04 +0900

    add info

--- activity_script.js
+++ activity_script.js
@@ -25,6 +25,19 @@ $(function () {
 		 });
 		 $('.mdl-layout--small-screen-only').hide();
 	});
+	$('#act_view_3').click(function(e) {
+		$('main').fadeOut(function () { 
+			$('#back-curtain')
+			.css({
+				'width' : $(window).width(),    // ウィンドウ幅
+				'height': $(window).height()    // 同 高さ
+			})
+			.show(function () { 
+				$('#act_3').fadeIn();
+			 });
+		 });
+		 $('.mdl-layout--small-screen-only').hide();
+	});
 	// ④暗幕と拡大画像を非表示
 	$('#back-curtain,button').on('click',function() {
 		$('.act_article').fadeOut( 
